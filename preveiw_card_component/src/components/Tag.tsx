5cde59baa7dccaa700970e29ba4a31d68c7ec960
yonatan afewerk <yonatanafewerk@gmail.com>
2023-11-16 08:43:15 +0300

    Development: react tag added

---
+++ preveiw_card_component/src/components/Tag.tsx
@@ -0,0 +1,12 @@
+import reactIcons from "../assets/react.svg";
+
+const Tag = () => {
+    return (
+        <div className="tag">
+            <img src={reactIcons} alt="react icon for tag" />
+            <p>R E A C T</p>
+        </div>
+    );
+};
+
+export default Tag;
